752e3cca03bd93bbdbcf5fb702f0a1f7750e4c12
Andrew Haigh <andrew@erebor.(none)>
2010-02-18 23:55:52 +1100

    Still learning my way around git. My last commit didn't include the modified scripts. Here they are now: * Changed models to be tag dependent album->disc->track based

--- bxt_description_generator/bxt_description_generator.py
+++ bxt_description_generator/bxt_description_generator.py
@@ -7,59 +7,131 @@ This software is licensed under version 2.0 of the WTFPL (see COPYING for detail
 
 import sys
 import os
+import os.path
 import re
 from models import *
 from jinja2 import Environment, PackageLoader
+from optparse import OptionParser
+try:
+    import imp
+    imp.reload(sys)
+except ImportError:
+    reload(sys)
 
-reload(sys)
 sys.setdefaultencoding("utf-8")
 
 def absolute_path(path):
-	''' Get the absolute path of a file, from this script '''
-	ROOT = os.path.dirname(os.path.realpath(__file__))
-	os.path.join(ROOT,path)
-	return os.path.join(ROOT,path)
+    """ Get the absolute path of a file, from this script """
+    root = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(root,path)
 
 def cleanify(name):
-	''' Strip out some things that don't play well in element ids '''
-	return re.sub(r"[\. ']", r'_', name)
+    """ Strip out some things that don't play well in element ids """
+    return re.sub(r"\W","_", name)
+
+def pretty_time(secs, format = "{h:01.0f}:{m:02.0f}:{s:02.0f}"):
+    """ Format a time according to a supplied format string """
+    hours = secs // 3600
+    secs -= 3600 * hours
+    mins = secs // 60
+    secs -= 60 * mins
+    return format.format(h = hours, m = mins, s = secs)
+
+def add_track(track, albums):
+    if track.album:
+        if track.album not in albums:
+            albums[track.album] = Album(track.album)
+        albums[track.album].attach_track(track)
+
+def add_scan(path, scan, scans):
+    guess = os.path.split(path)[1]
+    if guess not in scans:
+        scans[guess] = []
+    scans[guess].append(os.path.join(path, scan))
+
+def merge_scans(albums, scans):
+    for album in albums:
+        if album.name in scans:
+            album.attach_scans(scans[album.name])
+
+def merge_scans2(albums, scans):
+    for seq in scans.values():
+        for path in seq:
+            bits = path.split(os.sep)
+            name = bits.pop()
+            for bit in reversed(bits):
+                if bit in albums:
+                    albums[bit].attach_scans([name])
+                    break
 
 # are we running this standalone, rather than as a module?
 def main():
-	# Check to see if we have all the information we need
-	try:
-		#directory = unicode(sys.argv[1])
-		directory = sys.argv[1]
-		template = sys.argv[2]
-	except IndexError:
-		try:
-			import easygui
-			
-			# get directory from user
-			directory = None
-			while not directory:
-				directory = easygui.diropenbox('Where are the files?')
-			
-			# get template from user
-			template = None
-			while not template:
-				template = easygui.choicebox('What template do you want?', choices=os.listdir(absolute_path('templates')))
-		except ImportError:
-			sys.stderr.write("Usage: " + sys.argv[0] + " <directory> <template>\n")
-			exit()
-	
-	root = Folder(directory)
-	root.scan()
-	
-	env = Environment(loader=PackageLoader('bxt_description_generator', 'templates'))
-	env.filters['cleanify'] = cleanify
-	template = env.get_template(template)
-	output = template.render(root=root).encode("utf-8")
-	
-	try:
-		easygui.codebox(text=output)
-	except NameError:
-		print output
+    global parser
+    (options, args) = parser.parse_args()
+    # Check to see if we have all the information we need
+    try:
+        directory = args[0]
+        template = args[1]
+    except IndexError:
+        try:
+            import easygui
+            # get directory from user
+            while not directory:
+                directory = easygui.diropenbox("Where are the files?")
+            # get template from user
+            while not template:
+                template = easygui.choicebox(
+                    "What template do you want?",
+                    choices=os.listdir(absolute_path("templates")))
+        except ImportError:
+            sys.stderr.write("Usage: " + sys.argv[0] + " <directory> <template>\n")
+            return 1
+    
+    scans = {}
+    albums = {}
+    for branch, dirs, files in os.walk(directory):
+        for filename in files:
+            #if filename == ".albuminfo"
+            extension = os.path.splitext(filename)[1][1:]
+            if extension in ignoreFileExtensions:
+                continue
+            elif extension in imageFileExtensions:
+                add_scan(branch, filename, scans)
+            else:
+                add_track(Track(os.path.join(branch, filename)), albums)
+    
+    merge_scans2(albums, scans)
+    
+    root_node = albums.values()
+    root_node.sort(natural_sort)
+    #merge_scans(root_node, scans)
+    for album in root_node:
+        album.tidy()
+    
+    env = Environment(loader=PackageLoader("bxt_description_generator", "templates"))
+    env.filters["cleanify"] = cleanify
+    env.filters["pretty_time"] = pretty_time
+    template = env.get_template(template)
+    output = template.render(albums=root_node).encode("utf-8")
 
-if __name__ == '__main__':
-	sys.exit(main())
+    if options.outfile:
+        try:
+            f = open(options.outfile,"wb")
+            f.write(output)
+            f.close()
+        except IOError:
+            sys.stderr.write("Unable to write to {0}".format(options.outfile))
+            return 1
+    else:
+        try:
+            easygui.codebox(text=output)
+        except NameError:
+            sys.stdout.write(output)
+    return 0
+
+if __name__ == "__main__":
+    usage = "Usage: %prog <directory> [<template>] [options]"
+    parser = OptionParser(usage)
+    parser.add_option("-o","--outfile",dest="outfile",default=None,
+                      help="output filename",metavar="FILE")
+    sys.exit(main())
--- bxt_description_generator/models.py
+++ bxt_description_generator/models.py
@@ -1,156 +1,200 @@
 # -*- coding: utf-8 -*-
 
 import os
+import os.path
+import re as _re
 
 # extensions we associate with scans
-imageFileExtensions = ['jpg','JPG','jpeg','JPEG','png','PNG',]
+imageFileExtensions  = ['jpg','jpeg','png','tiff',]
 # extensions we know we don't want to deal with
-ignoreFileExtensions = ['mood','sfv','txt','nfo','m3u','ini','lnk','md5',]
+ignoreFileExtensions = ['mood','sfv','txt','nfo','m3u','ini','lnk','md5',
+                        'db','desktop','!ut',]
 
 def deduce_name(path):
-	'''
-	Deduce a file/directory name from a path
-	'''
-	head, tail = os.path.split(path)
-	return tail if tail else head
+    """ Deduce a file/directory name from a path """
+    head, tail = os.path.split(path)
+    return tail if tail else head
 
-def folder_sort(x, y):
-	return cmp(x.name, y.name)
+def deduce_discnumber(path):
+    """ Deduce a disc number from file naming """
+    return 1
 
-class ImageFileException:
-	pass
+def lazy_alphabetic(t):
+    """ Case insensitive (simplified) dictionary order sort """
+    t = t.upper()
+    t = t.strip()
+    if t.startswith("THE "):
+        t = t[4:]
+    t = t.replace("&","AND")
+    t = _re.sub("[^A-Z0-9 ]"," ",t)
+    t = _re.sub("\s{2,}"," ",t)
+    t = t.strip()
+    return t
+    
+# Comparisons
+def natural_sort(x, y):
+    return cmp(lazy_alphabetic(x.name), lazy_alphabetic(y.name))
 
-class UnknownFileException:
-	pass
+def track_sort(x, y):
+    return cmp(x.tracknumber, y.tracknumber)
 
-class Folder:
-	'''
-	Representation of a folder
+def coerce_to_number(x):
+    # Try for natural order first, i.e 1,2,3,...
+    # vs string order 1,10,11
+    try:
+        x = int(x.split("/")[0])
+    except Exception:
+        pass
+    return x
 
-	name: folder name
-	path: full path (absolute or relative) to the file from current directory
-	folders: a list of Folders directly under this one
-	files: a list of Files directly under this one
-	numScans: number of files in this folder that are scans
-	artists: list of artists
-	'''
-	def __init__(self, path, name=None):
-		self.name = name if name else deduce_name(path)
+# Signals
+class ImageFileException(Exception):
+    pass
 
-		self.path = path
-		self.folders = []
-		self.files = {
-			'music': [],
-			'scans': [],
-			'unknown': [],
-		}
-		self.bitrate = None
-		self.artists = []
-		self.extensions = []
-	
-	def __str__(self):
-		return self.name
+class UnknownFileException(Exception):
+    pass
 
-	def scan(self):
-		for name in os.listdir(self.path):
-			# Do we have a directory?
-			if os.path.isdir(os.path.join(self.path, name)):
-				self.folders.append(Folder(os.path.join(self.path, name)))
-			# Or do we have a file?
-			elif os.path.isfile(os.path.join(self.path, name)):
-				newFile = File(os.path.join(self.path, name))
-				try:
-					newFile.scan()
-					self.files['music'].append(newFile)
-				except ImageFileException:
-					self.files['scans'].append(newFile)
-				except UnknownFileException:
-					self.files['unknown'].append(newFile)
-		for folder in self.folders:
-			folder.scan()
+class Album:
+    """
+    Abstract representation of an album.
+    
+    name: album name
+    artists: list of contributing artists
+    ripper: album ripper
+    catalog_no: album catalog number
+    discs: dictionary of contained Discs
+    scans: list of contained scan filenames
+    length: total length of Tracks in seconds
+    size: total size of Tracks in bytes
+    average_bitrate: average bitrate of contained Tracks in kbit/s
+    dates: list of dates
+    extensions: list of Track file extensions
+    album_art: href of album art
+    """
+    def __init__(self, name):
+        self.name = name
+        self.ripper = "unknown"
+        self.artists = []
+        self.catalog_no = "PCCG-70036"
+        self.discs = {}
+        self.scans = []
+        self.length = 0
+        self.size = 0
+        self.average_bitrate = None
+        self.extensions = []
+        self.dates = []
+        self.album_art = "/home/andrew/Scribbles/py/bxt/bxt-py3/bdg/out/strikewitches-square.png"
+    def attach_disc(self, disc):
+        self.discs[disc.number] = disc
+    def attach_scans(self, scans):
+        self.scans.extend(scans)
+    def attach_track(self, track):
+        if not track.discnumber:
+            track.discnumber = deduce_discnumber(track.path)
+        if track.discnumber not in self.discs:
+            self.attach_disc(Disc(track.discnumber))
+        self.discs[track.discnumber].attach_track(track)
+    def tidy(self):
+        for disc in self:
+            disc.tidy()
+            self.length += disc.length
+            self.size += disc.size
+            disc.tracklist.sort(track_sort)
+            b_aggregate = 0; b_count = 0
+            for track in disc.tracklist:
+                if track.bitrate:
+                    b_aggregate += track.bitrate; b_count += 1
+                if track.artist and track.artist not in self.artists:
+                    self.artists.append(track.artist)
+                if track.extension and track.extension not in self.extensions:
+                    self.extensions.append(track.extension)
+                if track.date and track.date not in self.dates:
+                    self.dates.append(track.date)
+        self.artists.sort()
+        self.extensions.sort()
+        self.dates.sort()
+    def __iter__(self):
+        return iter(self.discs.values())
 
-		# calculate some stuff
-		self.calcBitrate()
-		self.calcExtensions()
-		self.calcArtists()
-		
-		# sort the lists
-		self.files['music'].sort(lambda x, y: cmp(x.track, y.track))
-		self.folders.sort(folder_sort)
-	
-	def calcBitrate(self):
-		aggregator = 0
-		count = 0
+class Disc:
+    """
+    Abstract representation of a disc
+    number: disc number
+    name: disc name
+    tracklist: list of child Tracks
+    length: total length of child tracks in seconds
+    size: total size of child tracks in bytes
+    """
+    def __init__(self, number, name = None):
+        self.number = number
+        self.name = name
+        self.tracklist = []
+    def attach_track(self, track):
+        self.tracklist.append(track)
+    def tidy(self):
+        self.length = 0
+        self.size = 0
+        for track in self:
+            self.length += track.length
+            self.size += track.size
+    def __iter__(self):
+        return iter(self.tracklist)
 
-		for file in self.files['music']:
-			if file.bitrate:
-				aggregator += file.bitrate
-				count += 1
+class Track:
+    """
+    Abstract representation of a track (essentially a file).
+    name: filename
+    path: full path
+    size: size of file in bytes
+    length: length of track in seconds
+    bitrate: bitrate of track in kbit/s
+    artist: contributing artist
+    album: album name
+    title: track title
+    tracknumber: track number
+    discnumber: disc number
+    date: supplied year/date of release
+    """
+    def __init__(self, path):
+        self.path = path
+        try:
+            import mutagen
+            _latermutagen = mutagen.version >= (1, 18)
+            if _latermutagen:
+                tags = mutagen.File(self.path, easy=True)
+            else:
+                tags = mutagen.File(self.path)
+        except Exception as e:
+            tags = None
+            import sys
+            sys.stderr.write("\"{0}\" appears to be malformed! Caught: {1!r}\n".format(self.path, e))
+        if not tags: # Bail out early
+            import sys
+            sys.stderr.write("I don't know what to do with \"{0}\"!\n".format(self.path))
+            raise TypeError
+        self.name = deduce_name(path)
+        self.size = os.path.getsize(self.path)
+        self.extension = os.path.splitext(self.name)[1][1:]
+        self.bitrate = 0; self.length = 0
+        try:
+            self.length  = tags.info.length
+            self.bitrate = tags.info.bitrate / 1000
+        except (AttributeError, ValueError, TypeError):
+            pass
+        if tags.__class__ == mutagen.mp3.MP3:
+            import mutagen.easyid3
+            tags = mutagen.easyid3.EasyID3(self.path)
+        for prop in ["title", "artist", "album", "tracknumber", "discnumber", "date"]:
+            if prop in tags:
+                setattr(self, prop, tags[prop][0])
+            else:
+                setattr(self, prop, None)
+        if not self.title:
+            self.title  = self.name.rsplit('.', 1)[0].decode("utf-8") # Everything but the extension
+        
+        self.tracknumber = coerce_to_number(self.tracknumber)
+        self.discnumber = coerce_to_number(self.discnumber)
+        self.date = coerce_to_number(self.date)
 
-		self.bitrate = aggregator / count if aggregator != 0 else None
-	
-	def calcArtists(self):
-		for file in self.files['music']:
-			if file.artist not in self.artists:
-				self.artists.append(file.artist)
-	
-	def calcExtensions(self):
-		for file in self.files['music']:
-			if file.extension not in self.extensions:
-				self.extensions.append(file.extension) 
-
-class File:
-	'''
-	Representation of a file
-
-	name: filename
-	path: full path (absolute or relative) to the file from current directory
-	extension: file extension
-	artist:
-	album:
-	title:
-	track:
-	'''
-	def __init__(self, path, name=None):
-		self.name = name if name else deduce_name(path)
-
-		self.path = path
-		try:
-			self.extension = os.path.splitext(self.name)[1][1:]
-		except IndexError:
-			self.extension = None
-		self.bitrate = None
-	
-	def __str__(self):
-		return self.name
-
-	def scan(self):
-		global imageFileExtensions
-		global ignoreFileExtensions
-
-		if self.extension in imageFileExtensions:
-			raise ImageFileException
-		elif self.extension in ignoreFileExtensions:
-			raise UnknownFileException
-		else:
-			import mutagen.mp3
-
-			try:
-				tags = mutagen.File(self.path)
-			except mutagen.mp3.HeaderNotFoundError:
-				import sys
-				sys.stderr.write("I can't pull information out of \"%s\"!\n" % self.path)
-			if not tags:
-				import sys
-				sys.stderr.write("I don't know what to do with \"%s\"!\n" % self.path)
-				raise UnknownFileException
-			else:
-				if isinstance(tags, mutagen.mp3.MP3):
-					self.bitrate = tags.info.bitrate / 1000
-
-					import mutagen.easyid3
-					tags = mutagen.easyid3.EasyID3(self.path)
-				self.artist = tags['artist'][0] if 'artist' in tags else None
-				self.album = tags['album'][0] if 'album' in tags else None
-				self.title = tags['title'][0] if 'title' in tags else self.name.rsplit('.', 1)[0].decode("utf-8") # everything but the extension
-				self.track = tags['tracknumber'][0] if 'tracknumber' in tags else None
+    def __str__(self):
+        return self.title if self.title else self.name
